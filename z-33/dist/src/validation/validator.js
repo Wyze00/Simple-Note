4456cd78de3530210895b50908a727700127d3dd
Wyze00 <nathantanusubroto15@gmail.com>
2025-05-25 13:15:29 +0700

    rm test and dist

--- z-33/dist/src/validation/validator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Validator = void 0;
-class Validator {
-    static validate(schema, request) {
-        return schema.parse(request);
-    }
-}
-exports.Validator = Validator;
